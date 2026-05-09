77ef2e45dd39394a0dddf3df76ba66c004d0a8ca
Andrew Baxter <>
2026-05-10 01:07:40 +0900

    Update cert

--- source/system_docker.nix
+++ source/system_docker.nix
@@ -1,33 +1,32 @@
-{}:
+{ }:
 let
   nixpkgsPath = <nixpkgs>;
-  buildSystem = (configuration: import
-    (nixpkgsPath + /nixos/lib/eval-config.nix)
-    { modules = [ configuration ]; });
+  buildSystem = (
+    configuration: import (nixpkgsPath + /nixos/lib/eval-config.nix) { modules = [ configuration ]; }
+  );
 in
-buildSystem
-  ({ pkgs, ... }:
+buildSystem (
+  { pkgs, ... }:
   {
     config = {
-      system.build.docker_image = pkgs.dockerTools.buildLayeredImage
-        {
-          name = "docker_image";
-          contents = [
-            (pkgs.cacert.override {
-              extraCertificateFiles = [
-                (pkgs.fetchurl {
-                  url = "https://storage.googleapis.com/zlr7wmbe6/spaghettinuum_s.crt";
-                  hash = "sha256-cg0EIXX05OrZDeZcgCkmMxfyJhBdEdBplYOJr3ET9fk=";
-                })
-              ];
-            })
-            (pkgs.buildEnv
-              {
-                name = "spaghettinuum";
-                paths = [ (import ./package.nix) ];
-                pathsToLink = [ "/bin" ];
+      system.build.docker_image = pkgs.dockerTools.buildLayeredImage {
+        name = "docker_image";
+        contents = [
+          (pkgs.cacert.override {
+            extraCertificateFiles = [
+              (pkgs.fetchurl {
+                url = "https://storage.googleapis.com/zlr7wmbe6/spaghettinuum_s.crt";
+                hash = "sha256-ZYtDF0muI971/5utIcVwjfQHflmvvNgYEg/b71XWgew=";
               })
-          ];
-        };
+            ];
+          })
+          (pkgs.buildEnv {
+            name = "spaghettinuum";
+            paths = [ (import ./package.nix) ];
+            pathsToLink = [ "/bin" ];
+          })
+        ];
+      };
     };
-  })
+  }
+)
